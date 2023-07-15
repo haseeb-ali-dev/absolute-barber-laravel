22cb5933362d64200114b7b3291208e7376cb4b1
haseeb-ali-dev <h45.busy@gmail.com>
2023-07-15 22:52:30 +0500

    left and background images position changed

--- resources/views/admin/general_setting/lpc.blade.php
+++ resources/views/admin/general_setting/lpc.blade.php
@@ -157,78 +157,6 @@
                         id="lpc_nav_color" class="form-control form-control-sm mx-3" style="width: 120px;">
                 </div>
 
-                <div class="form-group d-flex align-items-center">
-                    <label for="lpc_bg_type">Choose type of background</label>
-                    <span class="mx-3">
-                        <input type="radio" name="lpc_bg_type" value="color" class="lpc_bg_type"
-                            {{ $hexCode ? 'checked' : null }}>
-                        Color
-                    </span>
-                    <span class="mx-3">
-                        <input type="radio" name="lpc_bg_type" value="image" class="lpc_bg_type"
-                            {{ $hexCode ? null : 'checked' }}>
-                        Image
-                    </span>
-                </div>
-
-                <div class="color-div" style="display: {{ $hexCode ? 'block' : 'none' }}">
-                    <div class="form-group d-flex align-items-center">
-                        <label for="background">Background Color</label>
-                        <input type="color" value="{{ $general_setting->lpc_background }}" name="lpc_background_color"
-                            id="background" class="form-control form-control-sm mx-3" style="width: 120px;" min="1">
-                    </div>
-                </div>
-
-                <div class="image-div" style="display: {{ $hexCode ? 'none' : 'block' }}">
-                    <div class="form-group">
-                        <label for="">Existing Background Image</label>
-                        <div>
-                            <img src="{{ $bgSetting[0] == '#' ? 'https://placehold.co/400' : asset('public/uploads/' . $general_setting->lpc_background) }}"
-                                alt="" class="w_200" height="200">
-                        </div>
-                    </div>
-                    <div class="form-group">
-
-                        <div class="my-2">
-                            <span>
-                                <input type="radio" name="lpc_image_type" value="upload" class="lpc_image_type" checked>
-                                Upload yourself
-                            </span>
-                            <span class="mx-3">
-                                <input type="radio" name="lpc_image_type" value="choose" class="lpc_image_type">
-                                Choose from backgrounds
-                            </span>
-                        </div>
-
-                        <div class="upload-div">
-                            <label for="">Change Background Image</label>
-                            <div>
-                                <input type="file" name="lpc_background_image">
-                            </div>
-                        </div>
-
-
-                        <div class="choose-div" style="display: none">
-                            <select name="background_id" class="form-control select2">
-                                <option value=""></option>
-                                @foreach ($backgrounds as $row)
-                                    <option value="{{ $row->id }}"
-                                        data-image="{{ asset('public/uploads/' . $row->file) }}">
-                                        {{ $row->file }}
-                                    </option>
-                                @endforeach
-                            </select>
-                        </div>
-
-                    </div>
-                </div>
-                <div class="d-flex">
-                    <label class="switch mb-3">
-                        <input type="checkbox" name='lpc_centered' @if (isset($general_setting->lpc_centered) && $general_setting->lpc_centered=='1') checked @endif>
-                        <span class="slider"></span>
-                    </label>
-                    <span class="mx-2 p-1 mt-1 text-bold">Check if you want to align the Title and Paragraph Text to Center of Form?</span>
-                </div>
                 <div class="form-group d-flex align-items-center color-div">
                     <label class="col-md-2" for="background">Title Text Styling</label>
                     <input class="col-md-2" type="color" value="{{ $general_setting->lpc_title_color }}" name="lpc_title_color"
@@ -317,50 +245,128 @@
 
                 </div>
 
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group d-flex align-items-center">
+                            <div class="form-group">
+                                <label for="">Existing Left Side Image</label>
+                                <div>
+                                    <img src="{{ !isset($general_setting->lpc_left_bg) ? 'https://placehold.co/400' : asset('public/uploads/' . $general_setting->lpc_left_bg) }}"
+                                        alt="" class="w_200" height="200">
+                                </div>
+                            </div>
+                            <div class="form-group mx-4">
 
-                <div class="form-group d-flex align-items-center">
-                    <div class="form-group">
-                        <label for="">Existing Left Side Image</label>
-                        <div>
-                            <img src="{{ !isset($general_setting->lpc_left_bg) ? 'https://placehold.co/400' : asset('public/uploads/' . $general_setting->lpc_left_bg) }}"
-                                alt="" class="w_200" height="200">
-                        </div>
-                    </div>
-                    <div class="form-group mx-4">
+                                <div class="">
+                                    <span>
+                                        <input type="radio" name="lpc_left_image_type" value="upload_left" class="lpc_left_image_type" checked>
+                                        Upload yourself
+                                    </span>
+                                    <span class="mx-3">
+                                        <input type="radio" name="lpc_left_image_type" value="choose_left" class="lpc_left_image_type">
+                                        Choose from left side backgrounds
+                                    </span>
+                                </div>
 
-                        <div class="">
-                            <span>
-                                <input type="radio" name="lpc_left_image_type" value="upload_left" class="lpc_left_image_type" checked>
-                                Upload yourself
-                            </span>
-                            <span class="mx-3">
-                                <input type="radio" name="lpc_left_image_type" value="choose_left" class="lpc_left_image_type">
-                                Choose from left side backgrounds
-                            </span>
-                        </div>
+                                <div class="upload-left-div">
+                                    <label for="">Change Left Side Background Image</label>
+                                    <div>
+                                        <input type="file" name="lpc_left_bg">
+                                    </div>
+                                </div>
 
-                        <div class="upload-left-div">
-                            <label for="">Change Left Side Background Image</label>
-                            <div>
-                                <input type="file" name="lpc_left_bg">
+                                <div class="choose-left-div" style="display: none">
+                                    <select name="left_bg_id" class="form-control select2">
+                                        <option value=""></option>
+                                        @foreach ($left_backgrounds as $row)
+                                            <option value="{{ $row->id }}"
+                                                data-image="{{ asset('public/uploads/' . $row->file) }}">
+                                                {{ $row->file }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
                             </div>
                         </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="d-flex flex-column">
+                            <div class="form-group d-flex align-items-center mb-0">
+                                <label for="lpc_bg_type">Choose type of background</label>
+                                <span class="mx-3">
+                                    <input type="radio" name="lpc_bg_type" value="color" class="lpc_bg_type"
+                                        {{ $hexCode ? 'checked' : null }}>
+                                    Color
+                                </span>
+                                <span class="mx-3">
+                                    <input type="radio" name="lpc_bg_type" value="image" class="lpc_bg_type"
+                                        {{ $hexCode ? null : 'checked' }}>
+                                    Image
+                                </span>
+                            </div>
 
-                        <div class="choose-left-div" style="display: none">
-                            <select name="left_bg_id" class="form-control select2">
-                                <option value=""></option>
-                                @foreach ($left_backgrounds as $row)
-                                    <option value="{{ $row->id }}"
-                                        data-image="{{ asset('public/uploads/' . $row->file) }}">
-                                        {{ $row->file }}
-                                    </option>
-                                @endforeach
-                            </select>
+                            <div class="color-div" style="display: {{ $hexCode ? 'block' : 'none' }}">
+                                <div class="form-group d-flex align-items-center">
+                                    <label for="background">Background Color</label>
+                                    <input type="color" value="{{ $general_setting->lpc_background }}" name="lpc_background_color"
+                                        id="background" class="form-control form-control-sm mx-3" style="width: 120px;" min="1">
+                                </div>
+                            </div>
+
+                            <div class="image-div" style="display: {{ $hexCode ? 'none' : 'block' }}">
+                                <div class="form-group">
+                                    <label for="">Existing Background Image</label>
+                                    <div>
+                                        <img src="{{ $bgSetting[0] == '#' ? 'https://placehold.co/400' : asset('public/uploads/' . $general_setting->lpc_background) }}"
+                                            alt="" class="w_200" height="200">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+
+                                    <div class="my-2">
+                                        <span>
+                                            <input type="radio" name="lpc_image_type" value="upload" class="lpc_image_type" checked>
+                                            Upload yourself
+                                        </span>
+                                        <span class="mx-3">
+                                            <input type="radio" name="lpc_image_type" value="choose" class="lpc_image_type">
+                                            Choose from backgrounds
+                                        </span>
+                                    </div>
+
+                                    <div class="upload-div">
+                                        <label for="">Change Background Image</label>
+                                        <div>
+                                            <input type="file" name="lpc_background_image">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="choose-div" style="display: none">
+                                        <select name="background_id" class="form-control select2">
+                                            <option value=""></option>
+                                            @foreach ($backgrounds as $row)
+                                                <option value="{{ $row->id }}"
+                                                    data-image="{{ asset('public/uploads/' . $row->file) }}">
+                                                    {{ $row->file }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="d-flex">
+                                <label class="switch mb-3">
+                                    <input type="checkbox" name='lpc_centered' @if (isset($general_setting->lpc_centered) && $general_setting->lpc_centered=='1') checked @endif>
+                                    <span class="slider"></span>
+                                </label>
+                                <span class="mx-2 p-1 mt-1 text-bold">Check if you want to align the Title and Paragraph Text to Center of Form?</span>
+                            </div>
                         </div>
-
                     </div>
                 </div>
-
 
                 <button type="submit" class="btn btn-success">Update Settings</button>
                 <br>
