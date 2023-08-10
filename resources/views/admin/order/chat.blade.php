37159baea67cabd3d59129b14d7ad4b04b069ad1
haseeb-ali-dev <h45.busy@gmail.com>
2023-08-11 02:35:24 +0500

    Reload to fetch new messages

--- resources/views/admin/order/chat.blade.php
+++ resources/views/admin/order/chat.blade.php
@@ -38,7 +38,7 @@
                         <form action="{{ route('admin.order.chat.store') }}" method="post">
                             @csrf
                             <input type="hidden" name="order_id" value="{{ Crypt::encrypt($id) }}">
-                            <textarea name="msg" class="form-control" placeholder="Start typing here..."></textarea>
+                            <textarea name="msg" class="form-control" placeholder="Start typing here..." id="message-compose-box"></textarea>
                             <button type="submit" class="btn btn-success float-right mt-1">Send</button>
                         </form>
 
@@ -47,4 +47,29 @@
             </div>
         </div>
     </div>
+    <script>
+        $(document).ready(function() {
+            var textareaActive = false; // Flag to track if textarea is focused/active
+
+            // Listen for textarea focus event
+            $('#message-compose-box').on('focus', function() {
+                textareaActive = true;
+            });
+
+            // Listen for textarea blur event
+            $('#message-compose-box').on('blur', function() {
+                textareaActive = false;
+            });
+
+            // Function to reload the page
+            function reloadPage() {
+                if (!textareaActive) {
+                    location.reload();
+                }
+            }
+
+            // Reload the page every 15 seconds
+            setInterval(reloadPage, 15000);
+        });
+    </script>
 @endsection
