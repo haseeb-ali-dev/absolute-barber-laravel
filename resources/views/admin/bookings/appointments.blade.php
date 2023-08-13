a11264af497ddc020e05f6680cc5dd778824d90e
haseeb-ali-dev <h45.busy@gmail.com>
2023-08-13 20:54:39 +0500

    Show appointments on calendar

--- resources/views/admin/bookings/appointments.blade.php
+++ resources/views/admin/bookings/appointments.blade.php
@@ -1,5 +1,22 @@
 @extends('admin.admin_layouts')
 @section('admin_content')
+    <style>
+        .username {
+            font-size: 12px;
+            padding-block: 3px;
+        }
+
+        .tooltip {
+            position: absolute;
+            display: none;
+            background-color: rgba(0, 0, 0, 0.7);
+            color: #FFF;
+            padding: 5px;
+            border-radius: 3px;
+            font-size: 12px;
+        }
+    </style>
+
     <h1 class="h3 mb-3 text-gray-800">Appointments</h1>
 
     <div class="card shadow mb-4">
@@ -76,6 +93,41 @@
         </div>
     </div>
 
+    @php
+        $events = $data->map(function ($event) {
+            $start = $event->date . ' ' . $event->time;
+            $formattedStart = Carbon\Carbon::parse($start)->format('Y-m-d H:i:s');
+
+            $color = '#FF5733';
+            switch ($event->status) {
+                case 'pending':
+                    $color = '#4e73df';
+                    break;
+                case 'cancel':
+                    $color = '#e74a3b';
+                    break;
+                case 'accept':
+                    $color = '#32a37a';
+                    break;
+            }
+
+            return [
+                'id' => $event->id,
+                'title' => $event->info,
+                'username' => $event->name,
+                'color' => $color,
+                'start' => $formattedStart,
+            ];
+        });
+    @endphp
+
+    {{-- Appointments Calender --}}
+    <div class="card shadow mb-4">
+        <div class="card-body">
+            <div id="calendar"></div>
+        </div>
+    </div>
+
     <div class="modal fade" id="edit_appointment" tabindex="-1" role="dialog" aria-labelledby="edit_appointmentLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -123,6 +175,52 @@
             $("#edit_appointment input[name='phone']").val($(this).data('phone'));
             // Show modal
             $("#edit_appointment").modal('show');
+        });
+    </script>
+    <link href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css' rel='stylesheet' />
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js'></script>
+    <script>
+        var events = @json($events);
+
+        $(document).ready(function() {
+            $('#calendar').fullCalendar({
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay,listWeek'
+                },
+                // defaultDate: '2023-08-01',
+                navLinks: true,
+                editable: true,
+                eventLimit: true,
+                timeFormat: 'h:mm A',
+                events: events,
+                eventTextColor: '#FFFFFF',
+                eventRender: function(event, element) {
+                    element.css('font-size', '14px');
+                    element.append('<div class="username">' + event.username + '</div>');
+                },
+                handleWindowResize: true,
+                aspectRatio: 1.35,
+                theme: 'bootstrap4',
+                eventMouseover: function(calEvent, jsEvent) {
+                    var tooltip = '<div class="tooltip">' + calEvent.title + '</div>';
+                    $('body').append(tooltip);
+                    $(this).mouseover(function(e) {
+                        $(this).css('z-index', 10000);
+                        $('.tooltip').fadeIn(100);
+                        $('.tooltip').fadeTo('10', 1.9);
+                    }).mousemove(function(e) {
+                        $('.tooltip').css('top', e.pageY + 10);
+                        $('.tooltip').css('left', e.pageX + 20);
+                    });
+                },
+                eventMouseout: function(calEvent, jsEvent) {
+                    $(this).css('z-index', 8);
+                    $('.tooltip').remove();
+                }
+            });
         });
     </script>
 @endsection
